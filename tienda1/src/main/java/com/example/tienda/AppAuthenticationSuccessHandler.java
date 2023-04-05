f46279d8a2fa80e1ad2cb76041b7e69b2df0acb5
joshuagonzalez6 <joshuagonzalez680@gmail.com>
2023-04-05 00:37:08 -0600

    portafolio

---
+++ tienda1/src/main/java/com/example/tienda/AppAuthenticationSuccessHandler.java
@@ -0,0 +1,17 @@
+package com.example.tienda;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+
+    
+ public class AppAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
+ protected void handle(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+    }
+}
+
---
+++ tienda1/src/main/java/com/example/tienda/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.example.tienda;
+
+import com.example.tienda.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigureAdpter{
+    
+    @Autowired
+    private UserService userDetailsService;
+    private final UserService UserDetailsService;
+ 
+    
+    @Bean 
+    public BCryptPasswordEncoder passwordEnconder(){ /*el BCryptPasswordEncoder se encripta la contraseña */
+        return new BCryptPasswordEncoder();    
+    }
+    @Bean
+    public UserService getUserService() {
+        return new UserService();
+    }
+    @Bean
+    DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEnconder());
+        daoAuthenticationProvider.setUserDetailsService(getUserService());
+        return daoAuthenticationProvider;
+    
+}
+@Bean
+public AuthenticationSuccessHandler appAuthenticationSuccessHandler() {
+    return new AppAunthenticationSuccessHandler ();
+}
+public SecurityConfig(UserService userPrincipalDetailsService){
+        this.UserDetailsService = userPrincipalDetailsService;
+}
+ @Override 
+ protected void condigure (AuthenticationMangerBuilder auth) {
+     auth.authenticationProvider(authenticationProvider());
+ }
+ 
+ @Override
+ protected void configure (HttpSecurity http) throws Exception {
+     http.authorizeRequests()
+             .antMatchers("/persona","/login")
+             .hasRole("ADMIN")
+             .antMatchers("/personaN","persona","/" ,"/login")   
+             .hasAnyRole("USER","VENDEDOR" ,"ADMIN")
+             .anyRequest().authenticated()
+             .and()
+             .formLogin();
+}
+
+    }
